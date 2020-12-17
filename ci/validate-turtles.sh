34839c64ed380aa8a94f98aae2e14826ac8538e8
Plamen Tarkalanov <plamen.tarkalanov@ontotext.com>
2020-12-17 17:10:43 +0200

    fix unzip to untar

--- ci/validate-turtles.sh
+++ ci/validate-turtles.sh
@@ -1,4 +1,4 @@
 wget https://github.com/TopQuadrant/shacl/archive/shacl-1.3.2.tar.gz
-unzip shacl-1.3.2.tar.gz
+tar -xf shacl-1.3.2.tar.gz
 shacl-1.3.2/bin/shaclvalidate.sh -datafile broilers/broiler.ttl -shapesfile model.shacl
 
