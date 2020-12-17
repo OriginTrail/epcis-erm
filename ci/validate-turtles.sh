c73168d9d3a130b1097e70c362a5503b7c1d1308
Plamen Tarkalanov <plamen.tarkalanov@ontotext.com>
2020-12-17 17:18:58 +0200

    add bash for permission denied

--- ci/validate-turtles.sh
+++ ci/validate-turtles.sh
@@ -1,4 +1,4 @@
 wget https://repo1.maven.org/maven2/org/topbraid/shacl/1.3.2/shacl-1.3.2-bin.zip
 unzip shacl-1.3.2-bin.zip
-shacl-1.3.2/bin/shaclvalidate.sh -datafile broilers/broiler.ttl -shapesfile model.shacl
+bash shacl-1.3.2/bin/shaclvalidate.sh -datafile broilers/broiler.ttl -shapesfile model.shacl
 
