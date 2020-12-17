aadd71c426348605add038c55ced5dd6c912ebb9
Plamen Tarkalanov <plamen.tarkalanov@ontotext.com>
2020-12-17 17:17:38 +0200

    fix shacl zip

--- ci/validate-turtles.sh
+++ ci/validate-turtles.sh
@@ -1,4 +1,4 @@
-wget https://github.com/TopQuadrant/shacl/archive/shacl-1.3.2.tar.gz
-tar -xf shacl-1.3.2.tar.gz
+wget https://repo1.maven.org/maven2/org/topbraid/shacl/1.3.2/shacl-1.3.2-bin.zip
+unzip shacl-1.3.2-bin.zip
 shacl-1.3.2/bin/shaclvalidate.sh -datafile broilers/broiler.ttl -shapesfile model.shacl
 
