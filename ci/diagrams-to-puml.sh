bcadc1e415acd757609dd51ee52568942fb5809c
Plamen Tarkalanov <plamen.tarkalanov@ontotext.com>
2020-12-17 15:22:04 +0200

    fix image tag

--- ci/diagrams-to-puml.sh
+++ ci/diagrams-to-puml.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --mount type=bind,source=$(pwd),target=/puml-doc/ttl/ ptarkalanov/rdf-puml-gen-ci:latest
+docker run --mount type=bind,source=$(pwd),target=/puml-doc/ttl/ ptarkalanov/rdf-puml-gen-ci:0.0.1
